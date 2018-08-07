b76ede6a39d4c9c8b693c82dcdd9c76b02775be1
Brady Ochse <bradyoch@gmail.com>
2018-08-07 00:41:41 -0400

    file server working mostly

--- server.py
+++ server.py
@@ -1,7 +1,9 @@
-import socket
+import socket, os
 
 HOST = "127.0.0.1"
 PORT = 9000
+
+SERVER_ROOT = "www/"
 
 RESPONSE_200 = ("HTTP/1.1 200 OK\r\n"
                 "Content-type: text/html\r\n"
@@ -17,22 +19,17 @@ RESPONSE_404 = ("HTTP/1.1 404 Not Found\r\n"
                 "Content-length: 9\r\n\r\n"
                 "Not Found")
 
-HTML_BODY_TEMP = ("<h1>Welcome to the server</h1>")
+class Request():
+    def __init__(self):
+        self.command = ""
+        self.file = ""
+        self.misc = []
 
-def get_request(conn):
-    request = {}
-    data = conn.recv(1024).decode().split("\r\n")
-
-    request["Request"] = data[0]
-    data = data[1:]
-
-    for s in data:
-        if not s:
-            continue
-        tag, info = s.split(":", 1)
-        request[tag] = info
-
-    return request
+    def parse_request(self, data):
+        data = data.decode().split("\r\n")
+        self.command, self.file, _ = data[0].split()
+        
+        self.misc = data[1:]
 
 def server_connect():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
@@ -42,8 +39,23 @@ def server_connect():
         conn, addr = server.accept()
         with conn:
             print("Connection from: ", addr)
-            request = get_request(conn)
-            conn.sendall(RESPONSE_200.format(0).encode())
+            request = Request()
+            request.parse_request(conn.recv(1024))
+            
+            print(request.command, request.file)
+            if request.command != "GET":
+                conn.sendall(RESPONSE_400.encode())
+                return
+
+            if request.file == "/":
+                request.file == "/index.html"
+            print(request.file)
+            request.file = SERVER_ROOT + request.file
+
+            with open(request.file, "rb") as f:
+                file_len = os.fstat(f.fileno()).st_size
+                conn.sendall(RESPONSE_200.format(file_len).encode())
+                conn.sendfile(f)
 
 def main():
     server_connect()
