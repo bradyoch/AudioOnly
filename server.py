f5fd0b181dd7672b6e621c1106bf1d6d84f7fdc5
Brady Ochse <bradyoch@gmail.com>
2018-08-07 23:42:46 -0400

    URL regex setup

--- server.py
+++ server.py
@@ -1,9 +1,13 @@
-import socket, os
+#!/usr/bin/env python3
+
+import socket, os, re
 
 HOST = "127.0.0.1"
 PORT = 9000
 
 SERVER_ROOT = "www/"
+URL_REGEX = ("^((https{0,1}://){0,1}(youtu.be/|www.youtube.com/watch\?v=)){0,1}"
+             "([a-zA-Z0-9_]{11})$")
 
 # use conn.sendfile after sending this response
 RESPONSE_200 = ("HTTP/1.1 200 OK\r\n"
@@ -39,16 +43,37 @@ class Request():
             self.file += "index.html"
         self.file = SERVER_ROOT + self.file
 
+        while "%" in self.args:
+            i = self.args.index("%")
+            self.args = self.args[:i] \
+                    + chr(int(self.args[i+1:i+3], 16)) \
+                    + self.args[i+3:]
+
 def handle_get(conn, file):
-    with open(file, "rb") as f:
-        file_len = os.fstat(f.fileno()).st_size
-        conn.sendall(RESPONSE_200.format(file_len).encode())
-        conn.sendfile(f)
+    try:
+        with open(file, "rb") as f:
+            file_len = os.fstat(f.fileno()).st_size
+            conn.sendall(RESPONSE_200.format(file_len).encode())
+            conn.sendfile(f)
+    except FileNotFoundError as err:
+        conn.sendall(RESPONSE_400.encode())
+        print(err)
 
 def handle_post(conn, args):
-    args = [tuple(x.split("=")) for x in args.split("&")]
-    handle_get(conn, SERVER_ROOT + "index.html")
-    return
+    args = [tuple(x.split("=", 1)) for x in args.split("&")]
+    match = None
+
+    for pair in args:
+        if pair[0] == "url":
+            match = re.fullmatch(URL_REGEX, pair[1])
+            if not match:
+                break
+
+    if match:
+        print("Request URL: ", match.groups())
+        handle_get(conn, SERVER_ROOT + "index.html")
+    else:
+        conn.sendall(RESPONSE_400.encode())
 
 def handle_connection(conn):
     request = Request()
@@ -65,6 +90,7 @@ def handle_connection(conn):
 
 def server_connect():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
+        server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         server.bind((HOST, PORT))
         server.listen(1)
 
