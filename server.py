68e306c4fd89106b4979c99c96f40bc08bfde8c7
Brady Ochse <bradyoch@gmail.com>
2018-08-14 16:16:41 -0400

    Added cache limits and fixed bug with empty request

--- server.py
+++ server.py
@@ -37,6 +37,9 @@ class Request():
 
     def parse_request(self, data):
         data = data.decode().split("\r\n")
+        if not data:
+            return
+
         self.command, self.file, _ = data[0].split()
 
         self.misc = data[1:-1]
@@ -70,7 +73,7 @@ def handle_post(conn, args):
 
     for pair in args:
         if pair[0] == "url":
-            match = re.fullmatch(URL_REGEX, pair[1])
+            match = re.match(URL_REGEX, pair[1])
             if not match:
                 return None
 
@@ -78,10 +81,27 @@ def handle_post(conn, args):
 
     return video_code
 
+def clean_cache(cache_dir, size):
+    dir_size = len(cache_dir)
+    if dir_size >= 10:
+        oldest_name = cache_dir[0]
+        oldest_time = os.stat("cache/" + oldest_name).st_atime
+
+        for i in range(1, dir_size):
+            stat = os.stat("cache/" + cache_dir[i])
+
+            if stat.st_atime < oldest_time:
+                oldest_name = cache_dir[i]
+                oldest_time = stat.st_atime
+
+    os.remove("cache/" + oldest_name)
+
 def fetch_video(conn, vid_code):
     cache_dir = os.listdir("cache/")
 
     if not (vid_code + ".mp3" in cache_dir):
+        clean_cache(cache_dir, 10)
+
         subprocess.run(["./youtube-dl", "-x", \
                         "--audio-format", "mp3", \
                         "-o", "cache/%(id)s.%(ext)s", \
@@ -95,7 +115,7 @@ def fetch_video(conn, vid_code):
 
 def handle_connection(conn):
     request = Request()
-    vid_code = None
+    vid_code = ""
 
     while True:
         request.parse_request(conn.recv(1024))
@@ -104,10 +124,9 @@ def handle_connection(conn):
             handle_get(conn, request.file)
         elif request.command == "POST":
             vid_code = handle_post(conn, request.args)
+            if vid_code:
+                fetch_video(conn, vid_code)
             break
-
-    if vid_code:
-        fetch_video(conn, vid_code)
 
     conn.close()
 
